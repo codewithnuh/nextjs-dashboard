49aa43a6356968aecbb7a15f1072865def6fb45b
Noor-ul-Hassan <109018221+N-U-H-WD@users.noreply.github.com>
2024-01-28 15:02:43 +0500

    tsconfig updated

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
@@ -27,6 +27,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
           <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
         </div>
       </div>
